533663ce3a6dc5a54a7e1153cb8a480752961941
Kareem Diaa Helal <kareemdiaahelal@gmail.com>
2025-05-06 20:57:01 +0300

    code of review class

--- src/models/Review.java
+++ src/models/Review.java
@@ -1,5 +1,24 @@
 package models;
 
 public class Review {
-    
+    private int reviewID;
+    private String review;
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+    public String getReview() {
+        return review;
+    }
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public void submitReview(){
+        
+    }
 }
+
